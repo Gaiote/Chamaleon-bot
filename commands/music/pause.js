324a4044a9465df5e02ad94b9998a5600a432048
Gaiote <82620711+Gaiote@users.noreply.github.com>
2021-07-22 16:18:10 -0300

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message) {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} -Você não está no mesmo canal de voz!`);
 
         if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
 
