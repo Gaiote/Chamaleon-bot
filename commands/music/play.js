56cc67179d8c9a0b02c1cb4d97f9b5ff30128270
Gaiote <82620711+Gaiote@users.noreply.github.com>
2021-07-22 16:15:40 -0300

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message, args) {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - Você não está no mesmo canal de voz!`);
 
         if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
 
