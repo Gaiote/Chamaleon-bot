0d923c6ab277275d0fcb295323c07d169bbc9cd2
Gaiote <82620711+Gaiote@users.noreply.github.com>
2021-07-22 16:21:46 -0300

    comando de pause

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -5,16 +5,16 @@ module.exports = {
     utilisation: '{prefix}pause',
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - Você não está em um canal de voz!`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} -Você não está no mesmo canal de voz!`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
+        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - Nenhuma música tocando no momento !`);
 
-        if (client.player.getQueue(message).paused) return message.channel.send(`${client.emotes.error} - The music is already paused !`);
+        if (client.player.getQueue(message).paused) return message.channel.send(`${client.emotes.error} - A musica já esta parada!`);
 
         const success = client.player.pause(message);
 
-        if (success) message.channel.send(`${client.emotes.success} - Song ${client.player.getQueue(message).playing.title} paused !`);
+        if (success) message.channel.send(`${client.emotes.success} - Song ${client.player.getQueue(message).playing.title} Música parada com sucesso !`);
     },
 };
