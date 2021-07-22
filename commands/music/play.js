b23671ec323635e1300b6c8824ab6931ece6333c
Gaiote <82620711+Gaiote@users.noreply.github.com>
2021-07-22 16:24:54 -0300

    comando de play

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,11 +5,11 @@ module.exports = {
     utilisation: '{prefix}play [name/URL]',
 
     execute(client, message, args) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - Você não está em um canal de voz!`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - Você não está no mesmo canal de voz!`);
 
-        if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
+        if (!args[0]) return message.channel.send(`${client.emotes.error} -Por favor, indique o título de uma música!`);
 
         client.player.play(message, args.join("1"), { firstResult: true });
     },
